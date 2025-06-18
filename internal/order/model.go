88ce1ba695b0497872bf325984f0f0323552bac9
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-06-17 22:36:32 -0400

    Add an event to notify seller in main

--- internal/order/model.go
+++ internal/order/model.go
@@ -1,9 +1,9 @@
 package order
 
 type Order struct {
-    ID        string   `json:"id"`
-    UserID    string   `json:"userId"`
-    ListingIDs []string `json:"listingIds"`
-    Total     float64  `json:"total"`
-    CreatedAt int64    `json:"createdAt"` // Unix timestamp
+    ID        string `json:"id"`
+    UserID    string `json:"userId"`
+    UserEmail string `json:"userEmail"`
+    SellerID  string `json:"sellerId"`
+    ItemID    string `json:"itemId"`
 }
