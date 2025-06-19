bd4df57c1e800e32e7e4cad6c88c2fc2be1e8bf1
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-06-19 04:37:35 +0000

    Refactor Service interface by removing unused methods and updating GetByID signature

--- internal/seller/service.go
+++ internal/seller/service.go
@@ -1,22 +1,8 @@
 package seller
 
+// Service defines seller operations
 type Service interface {
-    GetByID(id string) (Seller, bool)
-}
-
-type service struct {
-    store map[string]Seller
-}
-
-func NewService() Service {
-    return &service{
-        store: map[string]Seller{
-            "seller1": {ID: "seller1", Name: "Ravi's Kitchen", Email: "ravi@kitchen.com"},
-        },
-    }
-}
-
-func (s *service) GetByID(id string) (Seller, bool) {
-    seller, found := s.store[id]
-    return seller, found
+    Register(s Seller) error
+    GetByID(id string) (*Seller, error)
+    ListAll() []Seller
 }
