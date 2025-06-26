a6ffa9905814a78570b4b5ac7c9a017be5c73ea8
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-06-26 00:40:09 +0000

    Refactor Service interface to improve parameter naming and consistency

--- internal/order/service.go
+++ internal/order/service.go
@@ -1,9 +1,10 @@
-package order
-
-// Service defines order operations
+// internal/order/service.go
 type Service interface {
     Create(o Order) error
     GetByID(id string) (*Order, error)
-    ListByUser(userID string) ([]Order, error)
-    Accept(id string) error
+    ListByUser(email string) ([]Order, error)
+
+    Accept(id, callerEmail string) error   // ← 2 args
+    Complete(id, callerEmail string) error // ← 2 args
 }
+package order
