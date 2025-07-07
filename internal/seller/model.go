4319cba024d08a2441bf988dd8122d35017679fa
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-07-07 06:43:15 +0000

    Add Password field to Seller model for input handling

--- internal/seller/model.go
+++ internal/seller/model.go
@@ -4,6 +4,7 @@ type Seller struct {
     ID       string `json:"id"`
     Name     string `json:"name"`
     Email    string `json:"email"`
+    Password string `json:"password"` // accept on input, omitted when outputting
     Phone    string `json:"phone"`
     Verified bool   `json:"verified"`
 }
